a821e3734d493dffe08f6b2ab815ad9e35378e7c
koiclone <cheryljcruz1@gmail.com>
2022-04-18 20:11:50 -0500

    debugger

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express');
-
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const fs = require("fs");
+const path = require("path");
+const { query } = require("express");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
+//testing url for development only: https://intense-retreat-06879.herokuapp.com/api/animals
+
+//parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//parse incoming JSON data
+app.use(express.json());
+
+//makes all files in public path static files usable during live server
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+const { animals } = require("./data/animals");
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port 3001!`);
 });
